95442f340eb2fcd23823f97a6c156e2e041ec5d4
mulvaneya7 <mulvaneya7@gmail.com>
2020-03-16 14:32:31 -0700

    updated image upload

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 <?php
 if(isset($_POST['submit'])) {
     //the path to store the uploaded image
-    $target = "images/".basename($_FILES['image']['name']);
+    //$target = "images/".basename($_FILES['image']['name']);
 
     //connect to database
     $db = mysqli_connect("localhost", "root", "", "Food");
@@ -12,17 +12,31 @@ if(isset($_POST['submit'])) {
     $title = $_POST['title'];
     $recipe = $_POST['recipe'];
 
-    //store the submitted data into the database
-    $sql = "INSERT INTO recipe_cards (image, title, recipe_text) VALUES ('$image', '$title', '$recipe')";
-    mysqli_query($db, $sql);
-    header("Location: index.php");
+    $imageExt = explode('.', $image);
+    $imageActualExt = strtolower(end($imageExt));
 
-    //Confirm upload
-    if(move_uploaded_file($_FILES['image']['temp_name'], $target)) {
-        $msg = "Image uploaded successfully!";
-    } else {
-        $msg = "There was a problem in uploading your image!";
+    
+
+    $allowed = array('jpg', 'jpeg', 'png');
+
+    if(in_array($imageActualExt, $allowed)) {
+        $imageNameNew = uniqid('',true).".".$imageActualExt;
+        $imageDestination = 'images/'.$imageNameNew; 
+        //store the submitted data into the database
+        $sql = "INSERT INTO recipe_cards (image, title, recipe_text) VALUES ('$imageNameNew', '$title', '$recipe')";
+        mysqli_query($db, $sql);
+        //header("Location: index.php");
+
+        //Confirm upload
+        if(move_uploaded_file($_FILES['image']['tmp_name'], $imageDestination)) {
+            $msg = "Image uploaded successfully!";
+        } else {
+            $msg = "There was a problem in uploading your image!";
+        }
     }
+
+    echo $msg;
+    header('Location: index.php');
 } 
     
 ?>
