daa3edfd5e118fe6a65f7b2341bf87288009c292
mulvaneya7 <mulvaneya7@gmail.com>
2020-03-16 18:51:27 -0700

    row output added for cards

--- index.php
+++ index.php
@@ -13,27 +13,10 @@
     <!-- Header -->
     <?php include 'components/header.php'; ?>
     <!-- Display Card  -->
-    <?php include 'displaycard.php'; ?>
-    
-    <div class="container">
-      <div class ="row justify-content-center align-items-center">
-        <!-- FA Arrow left -->
-        <i class="fas fa-angle-double-left fa-3x"></i>
-          <!-- Card Component -->
-            <div class="card border-warning" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <p><a href="#" class="btn btn-warning">+</a>  add to library</p>
-                </div>
-            </div>
-          <!-- FA Arrow right -->
-          <i class="fas fa-angle-double-right fa-3x"></i>
-        </div>
-      </div>
+    <div class="row ml-5">
+      <?php include 'displaycard.php'; ?>
     </div>
-
+    
     <div class="jumbotron jumbotron-fluid mt-4">
       <div class="container">
 
